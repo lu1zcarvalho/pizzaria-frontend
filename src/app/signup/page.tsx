544ae92575f04a16beaf42b06e58d093de0bdb3c
Luiz Augusto <zcarvalho722@gmail.com>
2025-01-15 12:57:13 -0300

    Criando página de signup e dashboard

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,55 @@
+import styles from '@/app/page.module.scss'
+import logoImg from '/public/logo.svg'
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Signup() {
+    return (
+        <>
+            <div className={styles.containerCenter}>
+                <Image
+                    src={logoImg}
+                    alt="Logo da pizzaria"
+                />
+
+                <section className={styles.login}>
+                    <h1>Criando sua conta</h1>
+                    <form>
+                        <input
+                            type="name"
+                            required
+                            name="name"
+                            placeholder='Digite seu nome...'
+                            className={styles.input}
+                        />
+
+                        <input
+                            type="email"
+                            required
+                            name="email"
+                            placeholder='Digite seu email...'
+                            className={styles.input}
+                        />
+
+                        <input
+                            type="password"
+                            required
+                            name="password"
+                            placeholder='**********'
+                            className={styles.input}
+                        />
+
+                        <button type="submit" className={styles.button}>
+                            Cadastrar
+                        </button>
+                    </form>
+
+                    <Link href="/" className={styles.text}>
+                        Já possui uma conta? Faça o login
+                    </Link>
+
+                </section>
+            </div>
+        </>
+    )
+}
