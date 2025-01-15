83eb6e8d435944e748096abeed4edd507adf34df
Luiz Augusto <zcarvalho722@gmail.com>
2025-01-15 13:30:01 -0300

    consumindo API para criar usuário

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,35 @@ import styles from '@/app/page.module.scss'
 import logoImg from '/public/logo.svg'
 import Image from "next/image"
 import Link from "next/link"
+import { api } from "@/services/api"
+import { redirect } from 'next/navigation'
 
 export default function Signup() {
+    async function handleRegister(formData: FormData) {
+        "use server"
+
+        const name = formData.get("name")
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        if ( name === "" || email === "" || password === "") {
+            console.log("PREENCHA TODOS OS CAMPOS")
+            return;
+        }
+
+        try {
+            await api.post("/users", {
+                name,
+                email,
+                password
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
+        redirect("/")
+    }
+
     return (
         <>
             <div className={styles.containerCenter}>
@@ -14,7 +41,7 @@ export default function Signup() {
 
                 <section className={styles.login}>
                     <h1>Criando sua conta</h1>
-                    <form>
+                    <form action={handleRegister}>
                         <input
                             type="name"
                             required
