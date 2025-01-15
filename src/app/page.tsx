e361f43c5955c602e595cccd92cd35625f0665af
Luiz Augusto <zcarvalho722@gmail.com>
2025-01-15 12:49:26 -0300

    Criando página de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
 import styles from '@/app/page.module.scss'
+import logoImg from '/public/logo.svg'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.teste}>Teste Teste</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image
+          src={logoImg}
+          alt="Logo da pizzaria"
+        />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder='Digite seu email...'
+              className={styles.input}
+            />
+
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder='**********'
+              className={styles.input}
+            />
+
+            <button type="submit" className={styles.button}>
+              Acessar
+            </button>
+          </form>
+
+          <Link href="/signup" className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+
+        </section>
+      </div>
+    </>
   );
 }
