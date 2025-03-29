46cfe57aa0f477807493e777913080f45e5a1531
Luiz Augusto <zcarvalho722@gmail.com>
2025-03-29 19:39:16 -0300

    feat: criando e estilizando listagem dos pedidos

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -1,10 +1,21 @@
+"use client"
 import Link from 'next/link'
 import styles from './styles.module.scss'
 import Image from 'next/image'
 import logoImg from '/public/logo.svg'
 import { LogOutIcon } from 'lucide-react'
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from "next/navigation"
 
 export function Header(){
+    const router = useRouter();
+
+    async function handleLogout(){
+        deleteCookie("session", {path: "/"})
+
+        router.replace("/")
+    }
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
@@ -27,7 +38,7 @@ export function Header(){
                         Produto
                     </Link>
 
-                    <form>
+                    <form action={handleLogout}>
                         <button type='submit'>
                             <LogOutIcon size={24} color='#FFF'/>
                         </button>
